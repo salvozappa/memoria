6b2483c0a57a5eae3d5e538c8ceac870552bc00a
Salvatore Zappalà <salvatorezappala@fastmail.com>
2023-10-28 23:39:25 +0100

    Remove old comment

--- source/hello.c
+++ source/hello.c
@@ -1,8 +1,3 @@
-// Sound and music example. The code is public domain, the
-// wav file is from Warcraft 2 audio setup, the midi file
-// is from Simon the Sorcerer, the mus file is from Doom.
-//      /Mattias Gustavsson
-
 #include "dos.h"
 
 int main( int argc, char* argv[] ) {
